f6f5176bd09295ef20d9b338569808867b9376ac
memmedovnicatt <nicat.mammadov2022@khazar.org>
2025-07-24 16:27:51 +0400

    third commit

--- src/main/java/com/nicat/suman/configuration/WebConfig.java
+++ src/main/java/com/nicat/suman/configuration/WebConfig.java
@@ -10,6 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")  // frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
