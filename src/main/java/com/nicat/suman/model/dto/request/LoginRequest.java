2b59d8da8e4cbd12b9e3a56e3b1f7cddcdf2d775
memmedovnicatt <nicat.mammadov2022@khazar.org>
2025-08-15 13:24:44 +0400

    deleted validation for authenticate and register

--- src/main/java/com/nicat/suman/model/dto/request/LoginRequest.java
+++ src/main/java/com/nicat/suman/model/dto/request/LoginRequest.java
@@ -11,12 +11,12 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginRequest {
 
-    @NotEmpty(message = "Username must not be empty")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
-    @Column(columnDefinition = "VARCHAR(255)")
+//    @NotEmpty(message = "Username must not be empty")
+//    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+//    @Column(columnDefinition = "VARCHAR(255)")
     String username;
 
-    @Size(min = 5, max = 30, message = "Password must be between 3 adn 30 characters")
-    @NotEmpty(message = "Password cannot be empty")
+//    @Size(min = 5, max = 30, message = "Password must be between 3 adn 30 characters")
+//    @NotEmpty(message = "Password cannot be empty")
     String password;
 }
